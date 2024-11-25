9dbade48ed0ffed7346c9b8782dd24ce6997b02d
Ging1991 <carloscaballeromorel@gmail.com>
2024-11-24 21:34:02 -0300

    cambio en lectores

---
+++ Persistencia2/LectorGenerico.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Caballero.Persistencia {
+		
+	public class LectorGenerico<T> : LectorJSON {
+
+		private T dato;
+
+		public LectorGenerico(string carpeta, string archivo) {
+			Inicializar(carpeta, archivo);
+		}
+
+
+		protected T Leer() {
+			dato = JsonUtility.FromJson<T>(LeerJSON());
+			return dato;
+		}
+
+
+		protected void Guardar(T dato) {
+			GuardarJSON(JsonUtility.ToJson(dato));
+		}
+
+
+	}
+
+}
