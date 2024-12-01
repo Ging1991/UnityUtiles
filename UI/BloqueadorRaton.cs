a0930041fb7637ff6f913d7c75713299ac94a0ea
Ging1991 <carloscaballeromorel@gmail.com>
2024-12-01 00:14:24 -0300

    Borrar diamante antiguo

--- UI/BloqueadorRaton.cs
+++ UI/BloqueadorRaton.cs
@@ -13,20 +13,17 @@ namespace Ging1991.UI {
 		void Start() {
 			capaOriginal = gameObject.layer;
 			capaBloqueadora = LayerMask.NameToLayer(CAPA_BLOQUEADORA);
-			Debug.Log("Capa bloqueadora: " + capaBloqueadora);
 		}
 
 
 		public void Bloquear(bool debeBloquear) {
 			capaBloqueadora = LayerMask.NameToLayer(CAPA_BLOQUEADORA);
 			gameObject.layer = debeBloquear ? capaBloqueadora : capaOriginal;
-			Debug.Log(gameObject.layer);
 		}
 
 
 		public static void BloquearTodos(string grupo, bool debeBloquear) {
 			BloqueadorRaton[] bloqueadores = FindObjectsOfType<BloqueadorRaton>();
-			Debug.Log(bloqueadores.Length);
 			foreach (BloqueadorRaton bloqueador in bloqueadores) {
 				if (bloqueador.grupos.Contains(grupo)) {
 					bloqueador.Bloquear(debeBloquear);
