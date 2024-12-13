5be5c667ba52a3509b5a5388bff3d2bd92485a93
Ging1991 <carloscaballeromorel@gmail.com>
2024-12-13 02:34:02 -0300

    Fix contador

--- Contadores/Contador.cs
+++ Contadores/Contador.cs
@@ -4,41 +4,29 @@ namespace Ging1991.Contadores {
 
 	public class Contador : MonoBehaviour {
 
-		public void SetColor(Color color) {
+		public void SetValor(Color color, int valor, int valorMaxVisible, int valorMaxHabilitado) {
+
+			int indice = 0;
 
 			foreach (Transform hijo in transform) {
+
 				ContadorIndividual contador = hijo.GetComponent<ContadorIndividual>();
-				contador?.SetColor(color);
-			}
-		}
+				if (contador != null) {
 
+					contador.SetColor(color);
+					contador.SetValor(indice < valor);
 
-		public void SetValor(int cantidad) {
-			int index = 0;
-			foreach (Transform hijo in transform) {
-				ContadorIndividual contador = hijo.GetComponent<ContadorIndividual>();
-				contador?.SetValor(cantidad > index);
-				index++;
-			}
-		}
+					if (indice >= valorMaxHabilitado) {
+						contador.SetInhabilitado();
+					}
 
+					hijo.gameObject.SetActive(indice < valorMaxVisible);
 
-		public void SetCantidadMaxima(int cantidadVisible, int cantidadHabilitada) {
-			int index = 0;
-			Debug.Log(cantidadHabilitada);
-			foreach (Transform hijo in transform) {
-
-				// Manejar visibilidad
-				hijo.gameObject.SetActive(index < cantidadVisible);
-
-				// Manejar habilitación
-				ContadorIndividual contador = hijo.GetComponent<ContadorIndividual>();
-				if (contador != null && index >= cantidadHabilitada) {
-					contador.SetInhabilitado();
+					indice++;
 				}
-
-				index++;
+				
 			}
+
 		}
 
 
