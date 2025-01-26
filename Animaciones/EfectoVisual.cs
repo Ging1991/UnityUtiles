9c0e061316736aba082078b6222f383e708f2d70
Ging1991 <carloscaballeromorel@gmail.com>
2025-01-25 23:40:41 -0300

    Nube

--- Animaciones/EfectoVisual.cs
+++ Animaciones/EfectoVisual.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Ging1991.Animaciones {
 
@@ -8,6 +9,11 @@ namespace Ging1991.Animaciones {
 		public List<GameObject> efectos;
 
 		public void Animar(string codigo) {
+			Animar(codigo, Color.white);
+		}
+
+
+		public void Animar(string codigo, Color color) {
 			GameObject instancia = null;
 			foreach (GameObject efecto in efectos) {
 				if (efecto.GetComponent<Animador>().codigo == codigo) {
@@ -17,6 +23,7 @@ namespace Ging1991.Animaciones {
 			}
 			if (instancia != null) {
 				instancia.GetComponent<Animador>().Inicializar();
+				instancia.GetComponentInChildren<Image>().color = color;
 			} else {
 				Debug.LogError($"No se encontró un efecto con el código {codigo}");
 			}
