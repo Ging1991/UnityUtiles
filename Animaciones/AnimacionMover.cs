3835e266545a7f388b64273e660ac87fe14aa9e0
Ging1991 <carloscaballeromorel@gmail.com>
2025-02-26 02:15:10 -0300

    Sale casillas

---
+++ Animaciones/AnimacionMover.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace Ging1991.Animaciones {
+
+	public class AnimacionMover : Animacion {
+
+		private readonly float escalaInicial;
+		private readonly int duracion;
+		private readonly float escalaFinal;
+		private readonly Transform transformador;
+
+        public AnimacionMover(float escalaInicial, float escalaFinal, int salto, int duracion,
+				Transform transformador, ImagenMaster imagenMaster) :
+				base(imagenMaster, salto) {
+
+			this.transformador = transformador;
+			this.escalaInicial = escalaInicial;
+			this.escalaFinal = escalaFinal;
+			this.duracion = duracion;
+			this.transformador.localScale = new Vector3(escalaInicial, escalaInicial, escalaInicial);
+	    }
+
+
+		protected override void AvanzarCuadro() {
+			float crecimiento = (escalaFinal - escalaInicial) / duracion;
+			float nuevaEscala = Mathf.Clamp(transformador.localScale.x + crecimiento, escalaInicial, escalaFinal);
+			transformador.localScale = new Vector3(nuevaEscala, nuevaEscala, nuevaEscala);
+		}
+
+
+    }
+	
+}
--- Casillas/ICasillaObservador.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ging1991.Casillas {
-
-	public interface ICasillaObservador {
-
-		void PresionarCasilla(string codigo, bool valor);
-
-	}
-
-}
